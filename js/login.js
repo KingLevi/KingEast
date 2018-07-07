dae672b20a157c1b1c8854462bf374585748a9fb
KingLevi <1058769269@qq.com>
2018-07-07 12:31:11 +0800

    myfile

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
 function check() {
 	if (formregister.username.value !="wangdong") {
-		document.getElementById("lname").innerHTML = "账号是你名字~▽~";
+		document.getElementById("lname").innerHTML = "你名字全拼哦~▽~";
 		return false;
 	} else if (formregister.password.value != "0619") {
 		document.getElementById("lname").innerHTML = "";
-		document.getElementById("lpwd").innerHTML  = "密码是你生日(~▽~)";  
+		document.getElementById("lpwd").innerHTML  = "密码是你生日~▽~";  
 		return false;
 	} else {
 		document.getElementById("lpwd").innerHTML  = "";
